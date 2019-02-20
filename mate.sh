4b742150e798429a1f86cd34733713d7daa4c732
Muhammad Rafli <44805762+RabbitCL4Y@users.noreply.github.com>
2019-02-20 19:21:42 +0800

    Create mate.sh

---
+++ mate.sh
@@ -0,0 +1,83 @@
+toilet -f pagga KALSEL[E]xploit |lolcat
+echo "############################################################"|lolcat
+echo "   mm.           dM8"|lolcat
+echo "   YMMMb.       dMM8"|lolcat
+echo "    YMMMMb     dMMM'"|lolcat
+echo "     YMMMb   dMMMP"|lolcat
+echo "       YMMM  MMM'"|lolcat
+echo "          MbdMP"|lolcat
+echo "      .dMMMMMM.P   -=[DEFACE SCRIPT CREATOR]=-"|lolcat
+echo "     dMM  MMMMMMM  -=[Coded By Muhammad Rafli]=-"|lolcat
+echo "     8MMMMMMMMMMI  -=[Team : KALSEL[E]Xploit]=-"|lolcat
+echo "      YMMMMMMMMM   -=[Telegram : RabbitCL4Y IG]=-"|lolcat
+echo "        MMMMMMP"|lolcat
+echo "       MxM .mmm"|lolcat
+echo "############################################################"|lolcat
+echo      "#                                              #"
+echo      "#                 {1}.PERTAMBAHAN              #"
+echo      "#              ======================          #"
+echo      "#                 {2}.PERKALIAN                #"
+echo      "#              ======================          #"
+echo      "#                 {3}.PEMBAGIAN                #"
+echo      "#              ======================          #"
+echo      "#                 {4}.PENGURANGAN              #"
+echo      "#              ======================          #"
+echo      "#                 {5}.PEMANGKATAN              #"
+echo      "#              ======================          #"
+echo      "#                 {0}.EXIT                     #"
+echo      "################################################"
+read -p "KALSEL[E]Xploit": red
+
+if [ $red = 1 ] || [ $red = 1 ]
+then
+clear
+toilet -f pagga KALSEL[E]xploit |lolcat
+pkg install python2
+python2 tambah.py
+exit
+fi
+
+if [ $ref = 2 ] || [ $red = 2 ]
+then
+clear
+toilet -f pagga KALSEL[E]xploit |lolcat
+pkg install python2
+python2 kali.py
+exit
+fi
+
+if [ $red = 3 ] || [ $red = 3 ]
+then
+clear
+toilet -f pagga KALSEL[E]xploit |lolcat
+pkg install python2
+python2 bagi.py
+exit
+fi
+
+if [ $red = 4 ] || [ $red = 4 ]
+then
+clear
+toilet -f pagga KALSEL[E]xploit |lolcat
+pkg install python2
+python2 kurang.py
+exit
+fi
+
+if [ $red = 5 ] || [ $red = 5 ]
+then
+clear
+toilet -f pagga KALSEL[E]xploit |lolcat
+pkg install python2
+python2 pangkat.py
+exit
+fi
+
+if [ $red = 0 ] || [ $red = 0 ]
+then
+clear
+pkg install python2
+clear
+python2 woi.py
+exit
+fi
